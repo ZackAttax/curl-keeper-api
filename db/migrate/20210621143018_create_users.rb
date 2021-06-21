4da46a20a99e9512fe147ce25f9f3bd4a02e96f8
Zachary Williams <kcaz311@gmail.com>
2021-06-21 11:11:40 -0400

    added login controller

---
+++ db/migrate/20210621143018_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :curl_pattern
+      t.string :curl_type
+      t.string :porosity
+      t.string :density
+      t.string :width
+
+      t.timestamps
+    end
+  end
+end
